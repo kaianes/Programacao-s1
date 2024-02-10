1366de3f946453c46b1397936acdd88344b7fa3d
Kaiane <kaiane.cordeiro@sou.inteli.edu.br>
2024-02-10 10:16:13 -0300

    feat: html content
    Limpeza de arquivos não utilizados e criação de conteúdo html

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { IoIosSearch } from "react-icons/io";
+import './style.css';
+import { useState } from 'react';
+import api from "./services/api";
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    
+    
+    if(input === ''){
+      alert('Digite um CEP');
+      return;
+    }
+    if (input.length !== "" | 8) {
+      alert('CEP inválido');
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("")
+    }
+    catch{
+      alert('Houve um erro.');
+      setInput('');
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <h1 className="title">Buscador CEP</h1>
+
+    <div className="containerInput">
+      <input type="text" placeholder="Digite o CEP"
+      value={input}
+      onChange={(event) => setInput(event.target.value)}
+      />
+      <button className="buttonSearch" onClick={handleSearch}>
+      <IoIosSearch />
+      </button>
+    </div>
+
+
+    {Object.keys(cep).length > 0 && (
+      <main className="main">
+        <h2>CEP: {cep.cep}</h2>
+        <span>Logradouro: {cep.logradouro}</span>  
+      </main>
+    )}
+    
+
     </div>
   );
 }
